25985cd61e125322edcbf1d76417cb232951b1ce
zimma08 <arthur.zimmermann@univille.br>
2025-08-18 23:29:57 +0000

    singleton

---
+++ designpatterns/src/main/java/br/univille/singleton/Singleton.java
@@ -0,0 +1,18 @@
+package br.univille.singleton;
+
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton(){
+
+    }
+    public static Singleton getInstance(){
+        if (instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+
+
+    }
+    
+}
