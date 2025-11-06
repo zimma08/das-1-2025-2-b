fe6cb25cb8a7486e26a9042848974952a21b7f2a
Arthur Winter Zimmermann <ARTHUW01@mail.nidec.com>
2025-11-06 19:52:13 -0300

    falta coisa

---
+++ microkernel_interface/src/main/java/br/univille/microkernel_interface/MicrokernelInterfaceApplication.java
@@ -0,0 +1,13 @@
+package br.univille.microkernel_interface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrokernelInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrokernelInterfaceApplication.class, args);
+	}
+
+}
---
+++ microkernel_interface/src/test/java/br/univille/microkernel_interface/MicrokernelInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.microkernel_interface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrokernelInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
